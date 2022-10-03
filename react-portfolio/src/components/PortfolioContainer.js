d8502f0b2d33e6e991acafcfac114d4792d0b0a9
Jason South <jason.south@me.com>
2022-10-02 21:43:32 -0500

    updated pages

--- react-portfolio/src/components/PortfolioContainer.js
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import '../../src/App.css';
 import NavTabs from './Navbar';
 import About from './pages/About';
-import Blog from './pages/Portfolio';
-import Home from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
@@ -11,13 +11,13 @@ export default function PortfolioContainer() {
 
   const renderPage = () => {
     if (currentPage === 'About') {
-      return <Home />;
-    }
-    if (currentPage === 'Portfolio') {
       return <About />;
     }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
     if (currentPage === 'Resume') {
-      return <Blog />;
+      return <Resume />;
     }
     return <Contact />;
   };
